80c9fd40e5bdddba7670ac48f9bb0d91a5e1a823
Brandon Davis <bcdavis1990@yahoo.com>
2022-03-17 13:12:26 -0400

    Updated App. JS/Working on Project.pug
    Continue to update Project.Pug file

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
 const express = require("express");
 const data = require("./data.json");
 const app = express();
+const portListen = 3000;
 
+/*
+ * Setting the view engine to "Pug"
+ */
 app.set("view engine", "pug");
 
-app.use(express.static("./public"));
-app.use(data);
+/*
+ * Serving the Static Files
+ */
+app.use("/static", express.static("public"));
+
+/*
+ * Routes
+ */
+
+app.get("/", (req, res) => {
+  res.locals.data = data.projects;
+  res.render("index", { data });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/project/:id", (req, res) => {
+  const id = req.params.id;
+  const project = data.projects[id];
+  if (project) {
+    res.render("project", { project });
+  } else {
+    const err = new Error();
+    err.status = 404;
+    err.message = `Project ${id} does not exist`;
+    next(err);
+  }
+});
+
+/*
+ *404 Error Handler
+ */
+app.use((req, res, next) => {
+  const err = new Error(`Oops! Looks like that page doesn't exist!`);
+  err.status = 404;
+  next(err);
+});
+/*
+ * Global Error Handler
+ */
+
+/*
+ * Setting up the port - Port number can be changed in const variable at the top
+ */
+app.listen(portListen, () => {
+  console.log(`The app is listening on port ${portListen}`);
+});
